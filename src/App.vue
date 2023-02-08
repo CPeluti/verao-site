e55a5dbc569568da8eba1f61852d764f281ca956
Caio Peluti <cpeluti@gmail.com>
2023-02-07 21:32:32 -0300

    Projeto iniciado

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from "./components/header.vue"
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Header />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-gray-500 h-12">
+        <img src="@/assets/logo_unb.png" />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
