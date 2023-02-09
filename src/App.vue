1dfafd6d866c4bfeb0d9973d877703c0f16f7ddd
Caio Peluti <cpeluti@gmail.com>
2023-02-09 16:01:56 -0300

    componente da main page adicionado

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,16 @@
 <script setup lang="ts">
 import Header from "./components/header.vue"
-import {MapPinIcon, CalendarIcon} from '@heroicons/vue/24/solid' 
+import MainPage from "./components/mainPage.vue"
 </script>
 
 <template>
   <header>
     <div class="wrapper">
       <Header class="z-10" />
-      <div class="relative container overflow-hidden h-screen">
-        <div class="text grid place-content-center gap-2">
-          <h1 class="text-5xl leading-tight text-white"> First Summer<br/> School<br/> on<br/> Robotic<br/> Mission<br/> Engineering</h1>
-          <div class="grid place-content-center gap-1">
-            <span>
-              <h3 class="text-white flex items-center justify-center gap-x-2">
-                <CalendarIcon class="w-6 h-6 inline"/>
-                02 Mar 2023 - 05 Mar 2023
-              </h3>
-            </span>
-            <span>
-              <h3 class="text-white flex items-center justify-center gap-x-2">
-                <MapPinIcon class="w-6 h-6 inline"/>
-                <span class="text-start">
-                  Departamendo de ciência da computação, UnB
-                </span>
-              </h3>
-            </span>
-          </div>
-        </div>
-        <img id="cover" class="z-0 h-full w-full object-cover" src="@/assets/cic_unb.jpg"/>
-      </div>
-      
-
+      <MainPage/>
     </div>
   </header>
 </template>
 
 <style scoped>
-.container{
-  margin: -5px, -10px, -10px, -5px;
-}
-.text{
-  @apply z-10 absolute w-full text-center top-1/2 left-1/2 leading-tight -translate-x-1/2 -translate-y-1/2
-}
-h1{
-  font-family: 'roboto';
-}
-#cover{
-  object-fit: cover;
-  transform: scale(1.3);
-
-  filter: blur(8px) brightness(60%);
-
-}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["index.html","./src/**/*.{vue,ts,html}"],
   theme: {
+    maxWidth:{
+      'xxs':'15rem'
+    },
     extend: {},
   },
   plugins: [],
